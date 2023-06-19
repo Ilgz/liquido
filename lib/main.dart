82503f08a23c04d8673d9767a9f5a92f6078d02e
Andrey Dubinsky <adreyka626@mail.ru>
2023-06-19 21:18:46 +0300

    Save TODo

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+// TODO: че лежит не понятно
 import 'package:flutter_shaders/flutter_shaders.dart';
 import 'package:liquido/fractal_pyramid.dart';
 
 void main() {
+  // TODO: че по консту?
   runApp(MaterialApp(debugShowCheckedModeBanner: false,home: const PyramidShader()));
 }
 
